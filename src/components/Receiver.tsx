695a077bff0cec6ba665efa43378226df4c5c172
Wadad Parker <wadadparker5@gmail.com>
2024-12-13 12:47:43 +0530

    refactor: use ref for rendering video

--- src/components/Receiver.tsx
+++ src/components/Receiver.tsx
@@ -1,59 +1,61 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react";
 
 const Receiver = () => {
-    
-    useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080');
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080');
         socket.onopen = () => {
             socket.send(JSON.stringify({
                 type: 'receiver'
             }));
         }
         startReceiving(socket);
-    }, []);
 
-    function startReceiving(socket: WebSocket) {
-        const page = document.querySelector("#page")
-        const video = document.createElement('video');
-        video.classList.add("border")
-        video.classList.add("rounded")
-        page?.appendChild(video);
+    return () => {
+        socket.close();
+    };
+  }, []);
 
-        const pc = new RTCPeerConnection();
-        pc.ontrack = (event) => {
-            video.srcObject = new MediaStream([event.track]);
-            video.play();
-        }
+  const startReceiving = (socket: WebSocket) => {
+    const pc = new RTCPeerConnection();
 
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
-            if (message.type === 'createOffer') {
-                pc.setRemoteDescription(message.sdp).then(() => {
-                    pc.createAnswer().then((answer) => {
-                        pc.setLocalDescription(answer);
-                        socket.send(JSON.stringify({
-                            type: 'createAnswer',
-                            sdp: answer
-                        }));
-                    });
-                });
-            } else if (message.type === 'iceCandidate') {
-                pc.addIceCandidate(message.candidate);
-            }
-        }
-    }
+    pc.ontrack = (event) => {
+        if (videoRef.current) {
+            videoRef.current.srcObject = event.streams[0];
+          }
+    };
+
+    socket.onmessage = async (event) => {
+      const message = JSON.parse(event.data);
+
+      if (message.type === "createOffer") {
+        await pc.setRemoteDescription(message.sdp);
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+        socket.send(
+          JSON.stringify({
+            type: "createAnswer",
+            sdp: answer,
+          })
+        );
+      } else if (message.type === "iceCandidate") {
+        await pc.addIceCandidate(message.candidate);
+      }
+    };
+  };
 
   return (
-    <div id="page" className="flex flex-col justify-center items-center h-screen">
-        <h1 className="text-3xl">Receiver</h1>
-        {/* <video ref={videoRef} autoPlay playsInline className="border rounded"></video> */}
-        {/* <button 
-        // onClick={startSendingVideo}
-        className="border rounded px-3 py-1.5 text-3xl bg-black text-white border-black hover:bg-white hover:text-black transition-all ease-linear">
-            Receiver
-        </button> */}
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl mb-4">Receiver</h1>
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          className="border rounded"
+        />
     </div>
-  )
-}
+  );
+};
 
-export default Receiver
+export default Receiver;
